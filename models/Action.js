18c4ed15ca5c6c4ff3879cd2fdce4d7fb48a05ec
ChristianL01 <locke.christian1@gmail.com>
2022-11-07 16:34:58 -0600

    add action, item models

--- models/Action.js
+++ models/Action.js
@@ -1,6 +1,12 @@
 const mongoose = require('../db/connection');
 
-const ActionSchema = new mongoose.Schema();
+const ActionSchema = new mongoose.Schema(
+    {
+        name: String,
+        description: String,
+        type: String
+    }
+);
 
 const Action = mongoose.model('Action', ActionSchema);
 module.exports = Action;
--- models/Item.js
+++ models/Item.js
@@ -1,6 +1,12 @@
 const mongoose = require('../db/connection');
 
-const ItemSchema = new mongoose.Schema();
+const ItemSchema = new mongoose.Schema({
+	name: String,
+	description: String,
+	type: String,
+	stat: String,
+	action: [{ type: mongoose.Schema.Types.ObjectId, rel: 'Action' }],
+});
 
 const Item = mongoose.model('Item', ItemSchema);
 module.exports = Item;
