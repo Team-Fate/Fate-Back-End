6280df68afe710a1b20bbdcff4ae7481c1a26f9e
BrandonBishop84 <brandonnovember@gmail.com>
2022-11-07 12:15:46 -0800

    create event&npc schema

--- models/NPC.js
+++ models/NPC.js
@@ -1,6 +1,17 @@
 const mongoose = require('../db/connection');
 
-const NPCSchema = new mongoose.Schema();
+const NPCSchema = new mongoose.Schema({
+	name: String,
+	description: String,
+	image: String,
+	model: String,
+	stats: {
+		S: Int,
+		D: Int,
+		C: Int,
+		I: Int,
+	},
+});
 
 const NPC = mongoose.model('NPC', NPCSchema);
 module.exports = NPC;
