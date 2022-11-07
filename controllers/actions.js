cf62a97fe280e8464fd0909216b4f0965d902568
ChristianL01 <locke.christian1@gmail.com>
2022-11-07 16:48:06 -0600

    add actions, items controller

--- controllers/actions.js
+++ controllers/actions.js
@@ -5,6 +5,39 @@ const Action = require('../models/Action');
 const { handleValidateOwnership } = require('../middleware/custom_errors');
 
 // GET (index) /api/actions/
-router.get('/', requireToken, (req, res, next) => {});
+router.get('/', requireToken, (req, res, next) => {
+    Action.find({})
+    .then((action) => res.json(action))
+    .catch(next);
+});
+
+router.get('/:actionId', requireToken, (req, res, next) => {
+    const actionId = req.params.id;
+	Action.findById(actionId)
+		.then((action) => res.json(action))
+		.catch(next);
+});
+
+router.post('/', requireToken, (req, res, next) => {
+    const actionData = req.body;
+	Action.create(actionData)
+		.then((action) => res.status(201).json(action))
+		.catch(next);
+});
+
+router.patch('/:actionId', requireToken, (req, res, next) => {
+    const actionId = req.params.id;
+    const actionData = req.body;
+	Action.findOneAndUpdate({ _id : actionId }, actionData, { new: true })
+		.then((action) => res.json(action))
+		.catch(next);
+});
+
+router.delete('/:actionId', requireToken, (req, res, next) => {
+    const actionId = req.params.id;
+	Action.findOneAndDelete({ _id: actionId})
+		.then((action) => res.json(action))
+		.catch(next);
+});
 
 module.exports = router;
--- controllers/items.js
+++ controllers/items.js
@@ -5,6 +5,38 @@ const Item = require('../models/Item');
 const { handleValidateOwnership } = require('../middleware/custom_errors');
 
 // GET (index) /api/items/
-router.get('/', requireToken, (req, res, next) => {});
+router.get('/', requireToken, (req, res, next) => {
+	Item.find({})
+		.then((item) => res.json(item))
+		.catch(next);
+});
 
+router.get('/:itemId', requireToken, (req, res, next) => {
+	const itemId = req.params.id;
+	Item.findById(itemId)
+		.then((item) => res.json(item))
+		.catch(next);
+});
+
+router.post('/', requireToken, (req, res, next) => {
+	const itemData = req.body;
+	Item.create(itemData)
+		.then((item) => res.status(201).json(item))
+		.catch(next);
+});
+
+router.patch('/:itemId', requireToken, (req, res, next) => {
+	const itemId = req.params.id;
+	const itemData = req.body;
+	Item.findOneAndUpdate({ _id: itemId }, itemData, { new: true })
+		.then((item) => res.json(item))
+		.catch(next);
+});
+
+router.delete('/:itemId', requireToken, (req, res, next) => {
+	const itemId = req.params.id;
+	Item.findOneAndDelete({ _id: itemId })
+		.then((item) => res.json(item))
+		.catch(next);
+});
 module.exports = router;
