74fc37d0e051688fdb6f1929a5fc11b1ad5f8f6e
vlados552 <vlados552@gmail.com>
2022-11-07 12:07:03 -0800

    Add User and Character models

--- models/User.js
+++ models/User.js
@@ -1,6 +1,27 @@
 const mongoose = require('../db/connection');
 
-const UserSchema = new mongoose.Schema();
+const UserSchema = new mongoose.Schema({
+	firstName: String,
+	lastName: String,
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	role: {
+		type: String,
+		required: true,
+	},
+});
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
