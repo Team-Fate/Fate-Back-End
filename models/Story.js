70001b94950a97f18f1685fffa568442f5e7344b
vlados552 <vlados552@gmail.com>
2022-11-08 09:18:56 -0800

    Add require token and authorization validation

--- models/Story.js
+++ models/Story.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const StorySchema = new Schema({
 	name: String,
 	template: String,
-	cards: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+	cards: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Card' }],
 });
 
 const Story = mongoose.model('Story', StorySchema);
