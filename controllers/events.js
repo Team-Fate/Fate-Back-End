0da7f8eac1372096a446c2e99e17ba363621ad24
vlados552 <vlados552@gmail.com>
2022-11-08 11:37:15 -0800

    Add require token and authorization validation

--- controllers/events.js
+++ controllers/events.js
@@ -2,45 +2,61 @@ const express = require('express');
 const router = express.Router();
 const { requireToken } = require('../middleware/auth');
 const Event = require('../models/Event');
-const { handleValidateOwnership } = require('../middleware/custom_errors');
+const { handleValidateAuthorization } = require('../middleware/custom_errors');
 
-// GET (index) /api/events/
-router.get('/', (req, res, next) => {
-	Event.find({})
-		.then((events) => res.json(events))
-		.catch(next);
+// GET '/' which will list out all events (role: admin)
+router.get('/', requireToken, async (req, res, next) => {
+	try {
+		handleValidateAuthorization(req, 'admin');
+		const events = await Event.find();
+		res.status(200).json(events);
+	} catch (error) {
+		next(error);
+	}
 });
 
-// GET (event by id)
-router.get('/:id', (req, res, next) => {
-	const id = req.params.id;
-	Event.findById(id)
-		.then((events) => res.json(events))
-		.catch(next);
+// GET '/:eventId' which will list a specific event (role: user, admin)
+router.get('/:eventId', requireToken, async (req, res, next) => {
+	try {
+		const event = Event.findById(req.params.eventId);
+		res.status(200).json(event);
+	} catch (error) {
+		next(error);
+	}
 });
 
-// POST (create event)
-router.post('/', (req, res, next) => {
-	const eventData = req.body;
-	Event.create(eventData)
-		.then((events) => res.status(201).json(events))
-		.catch(next);
+// POST '/' which will add a new event and return it (role: admin)
+router.post('/', requireToken, async (req, res, next) => {
+	try {
+		handleValidateAuthorization(req, 'admin');
+		const event = await Event.create(req.body);
+		res.status(201).json(event);
+	} catch (error) {
+		next(error);
+	}
 });
 
-// PATCH (update event)
-router.patch('/:id', (req, res, next) => {
-	const id = req.params.id;
-	const eventData = req.body;
-	Event.findOneAndUpdate({ _id: id }, eventData, { new: true })
-		.then((events) => res.json(events))
-		.catch(next);
+// PATCH '/:eventId' which will update an event and return it (role: admin)
+router.patch('/:eventId', requireToken, async (req, res, next) => {
+	try {
+		handleValidateAuthorization(req, 'admin');
+		const event = Event.findByIdAndUpdate(req.params.eventId, req.body, {
+			new: true,
+		});
+		res.status(200).json(event);
+	} catch (error) {
+		next(error);
+	}
 });
-// DELETE (event by id)
-router.delete('/:id', (req, res, next) => {
-	const id = req.params.id;
-	Event.findByIdAndDelete(id)
-		.then(() => res.sendStatus(204))
-		.catch(next);
+// DELETE '/:eventId' which will delete an event and return list of all events (role: admin)
+router.delete('/:eventId', requireToken, async (req, res, next) => {
+	try {
+		handleValidateAuthorization(req, 'admin');
+		await Event.findByIdAndDelete(req.params.eventId);
+		res.sendStatus(204);
+	} catch (error) {
+		next(error);
+	}
 });
 
 module.exports = router;
