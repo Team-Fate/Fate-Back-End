28c988054fc2219ad7a4dd36a00cd510496afd8b
vlados552 <vlados552@gmail.com>
2022-11-08 12:04:36 -0800

    Add require token and authorization validation

--- controllers/npcs.js
+++ controllers/npcs.js
@@ -4,43 +4,57 @@ const { requireToken } = require('../middleware/auth');
 const NPC = require('../models/NPC');
 const { handleValidateOwnership } = require('../middleware/custom_errors');
 
-// GET (index) /api/npcs/
-router.get('/', (req, res, next) => {
-	NPC.find({})
-		.then((npcs) => res.json(npcs))
-		.catch(next);
+// GET '/' which will list out all npcs (role: admin)
+router.get('/', requireToken, async (req, res, next) => {
+	try {
+		const npcs = await NPC.find();
+		res.status(200).json(npcs);
+	} catch (error) {
+		next(error);
+	}
 });
 
-// GET (NPC by id)
-router.get('/:id', (req, res, next) => {
-	const id = req.params.id;
-	NPC.findById(id)
-		.then((npcs) => res.json(npcs))
-		.catch(next);
+// GET '/:npcId' which will list a specific NPC (role: user, admin)
+router.get('/:npcId', requireToken, async (req, res, next) => {
+	try {
+		const npc = await NPC.findById(req.params.npcId);
+		res.status(200).json(npc);
+	} catch (error) {
+		next(error);
+	}
 });
 
-// POST (create NPC)
-router.post('/', (req, res, next) => {
-	const npcData = req.body;
-	NPC.create(npcData)
-		.then((npcs) => res.status(201).json(npcs))
-		.catch(next);
+// POST '/' which will add a new NPC and return it (role: admin)
+router.post('/', requireToken, async (req, res, next) => {
+	try {
+		handleValidateAuthorization(req, 'admin');
+		const npc = await NPC.create(req.body);
+		res.status(201).json(npc);
+	} catch (error) {
+		next(error);
+	}
 });
 
-// PATCH (update NPC)
-router.patch('/:id', (req, res, next) => {
-	const id = req.params.id;
-	const npcData = req.body;
-	NPC.findOneAndUpdate({ _id: id }, npcData, { new: true })
-		.then((npcs) => res.json(npcs))
-		.catch(next);
+// PATCH '/:npcId' which will update a NPC and return it (role: admin)
+router.patch('/:npcId', requireToken, async (req, res, next) => {
+	try {
+		const npc = await NPC.findByIdAndUpdate(req.params.npcId, req.body, {
+			new: true,
+		});
+		res.status(200).json(npc);
+	} catch (error) {
+		next(error);
+	}
 });
-// DELETE (NPC by id)
-router.delete('/:id', (req, res, next) => {
-	const id = req.params.id;
-	NPC.findByIdAndDelete(id)
-		.then(() => res.sendStatus(204))
-		.catch(next);
+// DELETE '/:npcId' which will delete a NPC and return list of all cards (role: admin)
+router.delete('/:npcId', requireToken, async (req, res, next) => {
+	try {
+		handleValidateAuthorization(req, 'admin');
+		await NPC.findByIdAndDelete(req.params.npcId);
+		res.sendStatus(204);
+	} catch (error) {
+		next(error);
+	}
 });
 
 module.exports = router;
